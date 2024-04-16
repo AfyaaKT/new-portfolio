6bb224ff924f33d514555fc728a61c321dc2861c
unknown <afyaakhudur@gmail.com>
2024-04-16 06:01:24 +0300

    add reveal animation

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const burger = document.querySelector('.burger')
 const menu = document.querySelector('.header__sm-menu')
 
@@ -32,3 +34,24 @@ links.forEach(link => {
         menu.classList.remove('visible');
     });
 });
+
+
+// reveal animations
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+  const elmsToReveal = document.querySelectorAll('.reveal');
+
+  for (let i = 0; i < elmsToReveal.length; i++) {
+    const windowHeight = window.innerHeight;
+    const revealTop = elmsToReveal[i].getBoundingClientRect().top;
+    const revealPoint = 150;
+
+    if (revealTop < windowHeight - revealPoint) {
+      elmsToReveal[i].classList.add('active');
+    }
+    // else {
+    //   elmsToReveal[i].classList.remove('active');
+    // }
+  }
+}
